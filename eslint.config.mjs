f426ad869d39c0085b6d4888e45609686fe6a231
edsonfischborn <edsonfischborn@gmail.com>
2024-10-07 10:08:39 -0300

    feat: setup boilerplate

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,42 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-export default [{
-    files: ["**/*.ts"],
-}, {
+export default [
+  {
+    files: ['**/*.ts'],
+  },
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
+      'simple-import-sort': simpleImportSort,
+      prettier: eslintPluginPrettier,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'import',
+          format: ['camelCase', 'PascalCase'],
+        },
+      ],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+      curly: 'warn',
+      eqeqeq: 'warn',
+      'no-throw-literal': 'warn',
+      semi: 'warn',
+      quotes: ['error', 'single'],
+      'prettier/prettier': ['error', { singleQuote: true }], // Adicione a regra para o Prettier
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
-}];
+  },
+];
